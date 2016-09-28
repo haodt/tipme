c65e612d108843d518ab74d5ee3f7def89d8f031
Hao <haodt1990@gmail.com>
2016-09-29 00:23:17 +0700

    Initial Commit

---
+++ tipme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipme
+//
+//  Created by Hao on 9/29/16.
+//  Copyright © 2016 Hao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
